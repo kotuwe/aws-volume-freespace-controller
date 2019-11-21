a33f391890d07c8d0fcb23605a26d890d1276163
Lykov <marshcat@gmail.com>
2019-11-21 23:38:53 +0300

    - update

--- aws-volume-freespace-controller.py
+++ aws-volume-freespace-controller.py
@@ -3,9 +3,18 @@
 import subprocess
 import psutil
 
+freeSpaceLowerLimit = 7000
+
 def getFreeSpace():
     diskUsage = psutil.disk_usage('/')
-    diskFreeSpace = diskUsage.free / (1024 ** 3)
+    diskFreeSpace = diskUsage.free / (1024 ** 2)
     print('Current free space: ', diskFreeSpace)
+    return diskFreeSpace
 
-getFreeSpace()
+def checkFreeSpaceLimit():
+    if getFreeSpace() < freeSpaceLowerLimit:
+        print('Need to growup!')
+    else:
+        print('All done!')
+
+checkFreeSpaceLimit()
