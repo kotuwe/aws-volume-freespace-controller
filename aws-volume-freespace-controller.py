b987c923298e8205b9b673ca470ce37d3631ef19
Lykov <marshcat@gmail.com>
2019-11-21 23:35:06 +0300

    - update

--- aws-volume-freespace-controller.py
+++ aws-volume-freespace-controller.py
@@ -4,7 +4,7 @@ import subprocess
 import psutil
 
 def getFreeSpace():
-    diskUsage = psutil.disk_usage('/').
+    diskUsage = psutil.disk_usage('/')
     diskFreeSpace = diskUsage.free / (1024 ** 3)
     print('Current free space: ', diskFreeSpace)
 
