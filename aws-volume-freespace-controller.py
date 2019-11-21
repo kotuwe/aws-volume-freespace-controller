006fe4281b6e86f32f7bbef39ab251358b78cb5e
Lykov <marshcat@gmail.com>
2019-11-21 23:34:06 +0300

    - add get free space logic

--- aws-volume-freespace-controller.py
+++ aws-volume-freespace-controller.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
 
 import subprocess
+import psutil
 
+def getFreeSpace():
+    diskUsage = psutil.disk_usage('/').
+    diskFreeSpace = diskUsage.free / (1024 ** 3)
+    print('Current free space: ', diskFreeSpace)
+
+getFreeSpace()
