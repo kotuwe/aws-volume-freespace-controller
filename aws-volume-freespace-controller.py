3881e3a4fef85cd82a429a570caae3fce1685c02
Lykov <marshcat@gmail.com>
2019-11-22 22:30:38 +0300

    - update

--- aws-volume-freespace-controller.py
+++ aws-volume-freespace-controller.py
@@ -20,7 +20,8 @@ def checkFreeSpaceLimit(freeSpace):
         return False
 
 def growUpVolume(freeSpace):
-    awsCurrentVolumeSize = subprocess.Popen("aws ec2 describe-volumes --volume-ids=vol-0b748cdfc3f2658b3 | jq -r '.Volumes' | jq -r '.[].Size'". stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    cmd = "aws ec2 describe-volumes --volume-ids=vol-0b748cdfc3f2658b3 | jq -r '.Volumes' | jq -r '.[].Size'"
+    awsCurrentVolumeSize = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = awsCurrentVolumeSize.communicate()
 
     print(output)
