0c0f137607d28f4f7c6fc0d2a853bdf57b45ff83
Andrey Lykov <kotuwe@MacBook-Pro-Andrey.local>
2020-01-31 14:13:01 +0300

    - fix config bug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import time
 import yaml
 
@@ -6,15 +6,15 @@ import processMemoryControl.procWatcher as procWatcher
 import volumeFreespaceControl.volumeWatcher as volumeWatcher
 
 def main():
-    processThread = procWatcher.WatcherThread(config["processMemoryControl"])
+    processThread = procWatcher.WatcherThread(self.config["processMemoryControl"])
     processThread.start()
 
-    volumeThread = volumeWatcher.WatcherThread(config["volumeFreespaceControl"])
+    volumeThread = volumeWatcher.WatcherThread(self.config["volumeFreespaceControl"])
     volumeThread.start()
 
 
 
 if __name__ == "__main__":
     with open("/etc/centry.yml", "r") as configFile:
-        config = yaml.load(configFile, Loader=yaml.FullLoader)
+        self.config = yaml.load(configFile, Loader=yaml.FullLoader)
     main()
