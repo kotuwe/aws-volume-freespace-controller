ecfd70f89525399478450fa4829482839d1180cc
Andrey Lykov <kotuwe@MacBook-Pro-Andrey.local>
2020-01-31 14:13:31 +0300

    - rollback last commit

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import processMemoryControl.procWatcher as procWatcher
 import volumeFreespaceControl.volumeWatcher as volumeWatcher
 
 def main():
-    processThread = procWatcher.WatcherThread(self.config["processMemoryControl"])
+    processThread = procWatcher.WatcherThread(config["processMemoryControl"])
     processThread.start()
 
-    volumeThread = volumeWatcher.WatcherThread(self.config["volumeFreespaceControl"])
+    volumeThread = volumeWatcher.WatcherThread(config["volumeFreespaceControl"])
     volumeThread.start()
 
 
 
 if __name__ == "__main__":
     with open("/etc/centry.yml", "r") as configFile:
-        self.config = yaml.load(configFile, Loader=yaml.FullLoader)
+        config = yaml.load(configFile, Loader=yaml.FullLoader)
     main()
